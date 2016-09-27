bd209b7ad9ff6d281be47b70917e5753c995b83f
Cody Condon <codycondon50@gmail.com>
2016-09-27 13:19:41 -0400

    work with protocols enums and extensions

--- Swift Documentation Exercises.playground/Contents.swift
+++ Swift Documentation Exercises.playground/Contents.swift
@@ -537,6 +537,92 @@ case let .broken(message):
 
 
 //Protocols
+protocol ExampleProtocol {
+    var simpleDescription: String {get}
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class"
+    var anotherProperty: Int = 69105
+    
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted"
+    }
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+
+
+enum ExampleEnum: ExampleProtocol {
+    case Base, Adjusted
+    var simpleDescription: String {
+        return self.getDescription()
+    }
+    func getDescription() -> String {
+        switch self {
+        case .Base:
+            return " A simple description"
+        case .Adjusted:
+            return " Adjusted description"
+        }
+    }
+    
+    mutating func adjust() {
+        self = ExampleEnum.Adjusted
+    }
+}
+
+var c = ExampleEnum.Base
+c.adjust()
+
+let cDescription = c.simpleDescription
+
+
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    
+    mutating func adjust() {
+        self += 42
+    }
+}
+
+
+print(7.simpleDescription)
+
+extension Double: ExampleProtocol {
+    var simpleDescription: String {
+        return " The number \(self)"
+    }
+    
+    var absoluteValue: Double {
+        return fabs(self)
+    }
+    
+    mutating func adjust() {
+        
+    }
+}
+
+var double: Double = -12.34
+double.simpleDescription
+double.absoluteValue
 
 
 
